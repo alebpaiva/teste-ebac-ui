8f074ea0e3af7e97af6911c7a9bdc5f61ea4c99d
Alexandre Paiva <alexandrecabarral@hotmail.com>
2024-03-07 18:29:43 -0300

    massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -14,18 +14,36 @@ describe('Funcionalidade: produtos', () => {
 
     });
 
-it.only('Deve buscar um produto com sucesso', () => {
+it('Deve buscar um produto com sucesso', () => {
     let produto = 'Pierce Gym Short'
     produtosPage.buscarProduto(produto)
     cy.get('.product_title').should('contain' , produto)
 });
 
 it('Deve visitar a pagina do produto', () => {
-    
+    produtosPage.visitarProduto('aether-gym-pant')
 });
 
 it('Deve adicionar o produto no carrinho', () => {
+    let qtd = 6
+    produtosPage.buscarProduto('Aero Daily Fitness Tee')
+    produtosPage.addProdutoCarrinho('M', 'Brown', qtd)
+
+    cy.get('.woocommerce-message').should('contain' , qtd + ' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
+});
+
+it.only('Deve adicionar o produto no carrinho buscando da massa de dados', () => {
+    cy.fixture('produtos').then(dados => {
+
+        produtosPage.buscarProduto(dados[2].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[2].tamanho, 
+            dados[2].cor, 
+            dados[2].quantidade )
+        cy.get('.woocommerce-message').should('contain' , dados[2].nomeProduto)
+    })
     
+
 });
 
 });
